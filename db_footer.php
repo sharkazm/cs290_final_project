01aa6ba145e1187d82669cddd12b9314935df786
sharkazm <sharkazm@onid.oregonstate.edu>
2015-06-07 16:30:58 -0400

    Create db_footer.php

---
+++ db_footer.php
@@ -0,0 +1,12 @@
+<!-- Names: Mike Sharkazy -->
+<!-- Class: CS 290 Spring 2015 -->
+<!-- Assignment: Final Project -->
+<!-- Title: Dating Website Database -->
+<!-- db_footer.php: footer file -->
+</div>
+<div class="panel panel-default">
+	<div class="panel-body col-lg-offset-4">
+		<p></p>
+	</div></div>
+</body>
+</html>
