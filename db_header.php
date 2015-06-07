bdd36949f623de7fe9631fae43a6032717108642
sharkazm <sharkazm@onid.oregonstate.edu>
2015-06-07 16:31:19 -0400

    Create db_header.php

---
+++ db_header.php
@@ -0,0 +1,23 @@
+<!-- Name: Mike Sharkazy -->
+<!-- Class: CS 290 Spring 2015-->
+<!-- Assignment: Final Project -->
+<!-- Title: Dating Website Database -->
+<!-- db_header.php: header file -->
+
+<!DOCTYPE html>
+<head>
+	<title>Dating Database</title>
+	<meta charset='UTF-8'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<!--<script type="text/javascript" src="js/bootstrap.js"></script>-->
+</head>
+<body>
+		<div class="jumbotron" id="menu">
+			<div class="page-header">
+  			<h1 align = "center"><font color="white">Dating Database<br /><small align = "center">Final project for CS 290</small></font></h1>
+			</div>
+				<p align = "center"><a class="btn btn-danger" href="db_logout.php" role="button" align="center">Logout</a>
+		</p>
+	</div>
+		<div class="col-lg-10 col-lg-offset-1" id="content" name="content">
