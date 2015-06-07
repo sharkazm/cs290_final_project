dd629a37552002d47794e00978ad0a57a96ee49c
sharkazm <sharkazm@onid.oregonstate.edu>
2015-06-07 16:33:47 -0400

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,18 @@
+<!--Code adapted from w3schools.com-->
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["image_submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+?>
